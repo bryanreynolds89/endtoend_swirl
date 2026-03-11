59536b3f689e6e5101e05fcfab41822347a33da0
Bryan Reynolds <bryan.reynolds@orgelwealth.com>
2026-03-11 20:41:30 +0000

    sprint 5

--- apps/api/src/api/agents/agents.py
+++ apps/api/src/api/agents/agents.py
@@ -9,16 +9,19 @@ from api.agents.utils.utils import format_ai_message
 from pydantic import BaseModel, Field
 from typing import List
 
+from litellm import completion
+
+
 class ToolCall(BaseModel):
     name: str
     arguments: dict
+
 
 ### QnA Agent Response Model
 
 class RAGUsedContext(BaseModel):
     id: str = Field(description="The ID of the item used to answer the question")
     description: str = Field(description="Short description of the item used to answer the question")
-    price: float | None = None
     
 class ProductQAAgentResponse(BaseModel):
     answer: str = Field(description="Answer to the question.")
@@ -34,14 +37,16 @@ class ShoppingCartAgentResponse(BaseModel):
     final_answer: bool = False
     tool_calls: List[ToolCall] = []
 
-### Warehosue Manager Agent Response Model
+
+### Warehouse Manager Agent Response Model
 
 class WarehouseManagerAgentResponse(BaseModel):
     answer: str = Field(description="Answer to the question.")
     final_answer: bool = False
     tool_calls: List[ToolCall] = []
 
-### Coordinator Response Model
+
+### Coordinator Agent Response Model
 
 class Delegation(BaseModel):
     agent: str
@@ -53,27 +58,21 @@ class CoordinatorAgentResponse(BaseModel):
     final_answer: bool = False
     answer: str = ""
 
-def _safe_trace_id() -> str:
-    current_run = get_current_run_tree()
-    if not current_run:
-        return ""
-    # prefer trace_id if present, otherwise fall back to run id
-    return str(getattr(current_run, "trace_id", "") or getattr(current_run, "id", "") or "")
 
-### Product QA Agent
+### Product QnA Agent
 
 @traceable(
     name="product_qa_agent",
     run_type="llm",
     metadata={"ls_provider": "openai", "ls_model_name": "gpt-4.1"}
 )
-def product_qa_agent(state) -> dict:
+def product_qa_agent(state, models=["gpt-4.1", "groq/llama-3.3-70b-versatile"]) -> dict:
 
-   template = prompt_template_config("api/agents/prompts/product_qa_agent.yaml", "product_qa_agent")
-   
-   prompt = template.render(
-      available_tools=state.product_qa_agent.available_tools
-   )
+   prompts = {}
+   for model in models:
+        prompts[model] = prompt_template_config("api/agents/prompts/product_qa_agent.yaml", model).render(
+            available_tools=state.product_qa_agent.available_tools
+        )
 
    messages = state.messages
 
@@ -82,25 +81,30 @@ def product_qa_agent(state) -> dict:
    for message in messages:
         conversation.append(convert_to_openai_messages(message))
 
-   client = instructor.from_openai(OpenAI())
+   client = instructor.from_litellm(completion)
 
-   response, raw_response = client.chat.completions.create_with_completion(
-        model="gpt-4.1",
-        response_model=ProductQAAgentResponse,
-        messages=[{"role": "system", "content": prompt}, *conversation],
-        temperature=0.5,
-   )
+   for model in models:
+        try:
+            response, raw_response = client.chat.completions.create_with_completion(
+                model=model,
+                response_model=ProductQAAgentResponse,
+                messages=[{"role": "system", "content": prompts[model]}, *conversation],
+                temperature=0.5,
+            )
+            break
+        except Exception as e:
+            print(f"Error with model {model}: {e}")
+            continue
 
    current_run = get_current_run_tree()
 
    if current_run:
-       current_run.metadata["usage_metadata"] = {
-           "input_tokens": raw_response.usage.prompt_tokens,
-           "output_tokens": raw_response.usage.completion_tokens,
-           "total_tokens": raw_response.usage.total_tokens
-       }
-
-   trace_id = _safe_trace_id()
+        current_run.metadata["usage_metadata"] = {
+            "input_tokens": raw_response.usage.prompt_tokens,
+            "output_tokens": raw_response.usage.completion_tokens,
+            "total_tokens": raw_response.usage.total_tokens,
+            # "cached_tokens": raw_response.usage.prompt_tokens_details.cached_tokens
+        }
 
    ai_message = format_ai_message(response)
 
@@ -124,15 +128,15 @@ def product_qa_agent(state) -> dict:
     run_type="llm",
     metadata={"ls_provider": "openai", "ls_model_name": "gpt-4.1"}
 )
-def shopping_cart_agent(state) -> dict:
+def shopping_cart_agent(state, models=["gpt-4.1", "groq/llama-3.3-70b-versatile"]) -> dict:
 
-   template = prompt_template_config("api/agents/prompts/shopping_cart_agent.yaml", "shopping_cart_agent") 
-
-   prompt = template.render(
-      available_tools=state.shopping_cart_agent.available_tools,
-      user_id=state.user_id,
-      cart_id=state.cart_id
-   )
+   prompts = {}
+   for model in models:
+        prompts[model] = prompt_template_config("api/agents/prompts/shopping_cart_agent.yaml", model).render(
+            available_tools=state.shopping_cart_agent.available_tools,
+            user_id=state.user_id,
+            cart_id=state.cart_id
+        )
 
    messages = state.messages
 
@@ -141,25 +145,30 @@ def shopping_cart_agent(state) -> dict:
    for message in messages:
         conversation.append(convert_to_openai_messages(message))
 
-   client = instructor.from_openai(OpenAI())
+   client = instructor.from_litellm(completion)
 
-   response, raw_response = client.chat.completions.create_with_completion(
-        model="gpt-4.1",
-        response_model=ShoppingCartAgentResponse,
-        messages=[{"role": "system", "content": prompt}, *conversation],
-        temperature=0.5,
-   )
+   for model in models:
+        try:
+            response, raw_response = client.chat.completions.create_with_completion(
+                model=model,
+                response_model=ShoppingCartAgentResponse,
+                messages=[{"role": "system", "content": prompts[model]}, *conversation],
+                temperature=0.5,
+            )
+            break
+        except Exception as e:
+            print(f"Error with model {model}: {e}")
+            continue
 
    current_run = get_current_run_tree()
 
    if current_run:
-       current_run.metadata["usage_metadata"] = {
-           "input_tokens": raw_response.usage.prompt_tokens,
-           "output_tokens": raw_response.usage.completion_tokens,
-           "total_tokens": raw_response.usage.total_tokens
-       }
-
-   trace_id = _safe_trace_id()
+        current_run.metadata["usage_metadata"] = {
+            "input_tokens": raw_response.usage.prompt_tokens,
+            "output_tokens": raw_response.usage.completion_tokens,
+            "total_tokens": raw_response.usage.total_tokens,
+            # "cached_tokens": raw_response.usage.prompt_tokens_details.cached_tokens
+        }
 
    ai_message = format_ai_message(response)
 
@@ -174,6 +183,7 @@ def shopping_cart_agent(state) -> dict:
       "answer": response.answer
    }
 
+
 ### Warehouse Manager Agent
 
 @traceable(
@@ -181,15 +191,13 @@ def shopping_cart_agent(state) -> dict:
     run_type="llm",
     metadata={"ls_provider": "openai", "ls_model_name": "gpt-4.1"}
 )
-def warehouse_manager_agent(state) -> dict:
+def warehouse_manager_agent(state, models=["gpt-4.1", "groq/llama-3.3-70b-versatile"]) -> dict:
 
-   template = prompt_template_config("api/agents/prompts/warehouse_manager_agent.yaml", "warehouse_manager_agent") 
-
-   prompt = template.render(
-      available_tools=state.warehouse_manager_agent.available_tools,
-      user_id=state.user_id,
-      cart_id=state.cart_id
-   )
+   prompts = {}
+   for model in models:
+        prompts[model] = prompt_template_config("api/agents/prompts/warehouse_manager_agent.yaml", model).render(
+            available_tools=state.warehouse_manager_agent.available_tools
+        )
 
    messages = state.messages
 
@@ -198,25 +206,30 @@ def warehouse_manager_agent(state) -> dict:
    for message in messages:
         conversation.append(convert_to_openai_messages(message))
 
-   client = instructor.from_openai(OpenAI())
+   client = instructor.from_litellm(completion)
 
-   response, raw_response = client.chat.completions.create_with_completion(
-        model="gpt-4.1",
-        response_model=WarehouseManagerAgentResponse,
-        messages=[{"role": "system", "content": prompt}, *conversation],
-        temperature=0.5,
-   )
+   for model in models:
+        try:
+            response, raw_response = client.chat.completions.create_with_completion(
+                model=model,
+                response_model=WarehouseManagerAgentResponse,
+                messages=[{"role": "system", "content": prompts[model]}, *conversation],
+                temperature=0.5,
+            )
+            break
+        except Exception as e:
+            print(f"Error with model {model}: {e}")
+            continue
 
    current_run = get_current_run_tree()
 
    if current_run:
-       current_run.metadata["usage_metadata"] = {
-           "input_tokens": raw_response.usage.prompt_tokens,
-           "output_tokens": raw_response.usage.completion_tokens,
-           "total_tokens": raw_response.usage.total_tokens
-       }
-
-   trace_id = _safe_trace_id()
+        current_run.metadata["usage_metadata"] = {
+            "input_tokens": raw_response.usage.prompt_tokens,
+            "output_tokens": raw_response.usage.completion_tokens,
+            "total_tokens": raw_response.usage.total_tokens,
+            # "cached_tokens": raw_response.usage.prompt_tokens_details.cached_tokens
+        }
 
    ai_message = format_ai_message(response)
 
@@ -231,7 +244,8 @@ def warehouse_manager_agent(state) -> dict:
       "answer": response.answer
    }
 
-### Intent Router Agent Node
+
+### Coordinator Agent
 
 
 @traceable(
@@ -239,11 +253,11 @@ def warehouse_manager_agent(state) -> dict:
     run_type="llm",
     metadata={"ls_provider": "openai", "ls_model_name": "gpt-4.1"}
 )
-def coordinator_agent(state):
+def coordinator_agent(state, models=["gpt-4.1", "groq/llama-3.3-70b-versatile"]):
 
-   template = prompt_template_config("api/agents/prompts/coordinator_agent.yaml", "coordinator_agent") 
-   
-   prompt = template.render()
+   prompts = {}
+   for model in models:
+        prompts[model] = prompt_template_config("api/agents/prompts/coordinator_agent.yaml", model).render()
 
    messages = state.messages
 
@@ -252,27 +266,32 @@ def coordinator_agent(state):
    for message in messages:
         conversation.append(convert_to_openai_messages(message))
 
-   client = instructor.from_openai(OpenAI())
+   client = instructor.from_litellm(completion)
 
-   response, raw_response = client.chat.completions.create_with_completion(
-        model="gpt-4.1",
-        response_model=CoordinatorAgentResponse,
-        messages=[{"role": "system", "content": prompt}, *conversation],
-        temperature=0.5,
-   )
+   for model in models:
+        try:
+            response, raw_response = client.chat.completions.create_with_completion(
+                model=model,
+                response_model=CoordinatorAgentResponse,
+                messages=[{"role": "system", "content": prompts[model]}, *conversation],
+                temperature=0.5,
+            )
+            break
+        except Exception as e:
+            print(f"Error with model {model}: {e}")
+            continue
 
    current_run = get_current_run_tree()
 
    if current_run:
-       current_run.metadata["usage_metadata"] = {
-           "input_tokens": raw_response.usage.prompt_tokens,
-           "output_tokens": raw_response.usage.completion_tokens,
-           "total_tokens": raw_response.usage.total_tokens
-       }
-   
-   trace_id = _safe_trace_id()
-
-
+        current_run.metadata["usage_metadata"] = {
+            "input_tokens": raw_response.usage.prompt_tokens,
+            "output_tokens": raw_response.usage.completion_tokens,
+            "total_tokens": raw_response.usage.total_tokens,
+            # "cached_tokens": raw_response.usage.prompt_tokens_details.cached_tokens
+        }
+        trace_id = str(getattr(current_run, "trace_id", current_run.id))
+    
    if response.final_answer:
       ai_message = [AIMessage(
          content=response.answer,
